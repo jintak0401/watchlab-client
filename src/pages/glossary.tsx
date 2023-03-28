a1589bb6e670a039f0fbc1b686a61d22b4d86689
jintak0401 <jintak0401@naver.com>
2023-03-28 22:54:43 +0900

    feat: 제목 및 설명 문구 추가 (#2)

--- src/pages/glossary.tsx
+++ src/pages/glossary.tsx
@@ -1,10 +1,14 @@
 import { dehydrate, QueryClient } from '@tanstack/react-query';
 import { GetServerSidePropsContext } from 'next';
 import { useRouter } from 'next/router';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import 'twin.macro';
 
 import { getGlossaryKey } from '@/hooks/rq/glossary';
 
 import CharPicker from '@/components/glossary/CharPicker';
+import InputSearch from '@/components/glossary/InputSearch';
 import GlossaryWrapper from '@/components/glossary/Wrapper';
 
 import { getGlossary } from '@/request/glossary';
@@ -20,15 +24,20 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
     props: {
       dehydratedState: dehydrate(queryClient),
+      ...(await serverSideTranslations(locale, ['glossary'])),
     },
   };
 }
 
 const GlossaryPage = () => {
   const { locale = 'en' } = useRouter();
+  const { t } = useTranslation('glossary');
 
   return (
     <GlossaryWrapper>
+      <h1 tw="font-cormor font-bold text-8xl text-white">{t('title')}</h1>
+      <h2 tw="font-cormor text-3xl text-white">{t('description')}</h2>
+      <InputSearch />
       <CharPicker />
     </GlossaryWrapper>
   );

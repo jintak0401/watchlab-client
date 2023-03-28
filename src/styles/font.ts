07b444aab9e5abc790457838381b9a4340cc34b7
jintak0401 <jintak0401@naver.com>
2023-03-28 22:52:51 +0900

    feat: 폰트 최적화를 위한 폰트 추가 (#2)
    - Cormorant Garamond 폰트 추가

---
+++ src/styles/font.ts
@@ -0,0 +1,7 @@
+import { Cormorant_Garamond } from 'next/font/google';
+
+export const cormorantGaramond = Cormorant_Garamond({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-cormorant-garamond',
+});

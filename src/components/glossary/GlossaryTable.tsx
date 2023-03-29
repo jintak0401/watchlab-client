68d0511cf31ca88bc165b79ed4f24f05b3596a5e
jintak0401 <jintak0401@naver.com>
2023-03-30 00:06:47 +0900

    feat: 단어 테이블 구현 (#2)

--- src/components/glossary/GlossaryTable.tsx
+++ src/components/glossary/GlossaryTable.tsx
@@ -1,26 +1,53 @@
 import { useAtom } from 'jotai';
-import tw from 'twin.macro';
+import { useEffect, useRef } from 'react';
+import tw, { css } from 'twin.macro';
 
-import { glossaryTableAtom } from '@/store/glossary';
+import Row from '@/components/glossary/Table/Row';
+
+import { glossaryFilteredWordsAtom, glossaryTableAtom } from '@/store/glossary';
 
 import Table from './Table/Table';
 
 const GlossaryTable = () => {
   const [tableShow] = useAtom(glossaryTableAtom);
+  const [filteredWords] = useAtom(glossaryFilteredWordsAtom);
+  const tableRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (tableRef.current) {
+      tableRef.current.scrollTop = 0;
+    }
+  }, [filteredWords]);
+
   return (
     <Table
+      ref={tableRef}
       css={[
-        tw`absolute top-full bg-white`,
-        {
-          width: 1000,
-          height: 500,
-        },
-        tableShow && {
-          transition: 'top 0.8s ease-in-out',
-          top: '50vh',
-        },
+        tw`absolute top-full`,
+        css`
+          width: 1000px;
+          height: calc(95vh - 470px);
+          overflow-y: auto;
+          &::-webkit-scrollbar {
+            display: none;
+          }
+        `,
+        tableShow &&
+          css`
+            transition: top 0.8s ease-in-out;
+            top: 470px;
+          `,
       ]}
-    />
+    >
+      {filteredWords?.map(({ word, description }, idx) => (
+        <Row
+          key={word}
+          css={[idx === filteredWords.length - 1 && tw`border-b-0`]}
+          word={word}
+          description={description}
+        />
+      ))}
+    </Table>
   );
 };
 

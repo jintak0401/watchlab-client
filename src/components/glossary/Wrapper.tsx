7dade5a6572f6d3acb95f7ffba647027162481d4
jintak0401 <jintak0401@naver.com>
2023-03-29 22:38:13 +0900

    feat: PropsWithChildren으로 변경 및 relative 추가 (#2)

--- src/components/glossary/Wrapper.tsx
+++ src/components/glossary/Wrapper.tsx
@@ -1,14 +1,10 @@
-import { ReactNode } from 'react';
+import { PropsWithChildren } from 'react';
 import 'twin.macro';
 
-interface Props {
-  children?: ReactNode;
-}
-
-const Wrapper = ({ children }: Props) => {
+const Wrapper = ({ children }: PropsWithChildren) => {
   return (
     <div tw="h-screen w-screen bg-glossary">
-      <div tw="flex h-full w-full flex-col items-center justify-center gap-6 bg-gradient-to-b from-black to-transparent">
+      <div tw="relative flex h-full w-full flex-col items-center justify-center gap-6 bg-gradient-to-b from-black to-transparent">
         {children}
       </div>
     </div>

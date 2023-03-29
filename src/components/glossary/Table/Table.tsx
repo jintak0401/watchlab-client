64f2cb0be5525d4ca278c5835dcd45d600be1466
jintak0401 <jintak0401@naver.com>
2023-03-30 00:06:24 +0900

    feat: 단어 테이블 구현 (#2)

--- src/components/glossary/Table/Table.tsx
+++ src/components/glossary/Table/Table.tsx
@@ -1,7 +1,17 @@
-import { PropsWithTwChildren } from 'react';
+import { ForwardedRef, forwardRef, PropsWithTwChildren } from 'react';
+import 'twin.macro';
 
-const Table = ({ children, ...rest }: PropsWithTwChildren) => {
-  return <table {...rest}>{children}</table>;
+const Table = (
+  { children, ...rest }: PropsWithTwChildren,
+  ref: ForwardedRef<HTMLDivElement>
+) => {
+  return (
+    <div ref={ref} tw="relative overflow-x-auto" {...rest}>
+      <table tw="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+        <tbody>{children}</tbody>
+      </table>
+    </div>
+  );
 };
 
-export default Table;
+export default forwardRef(Table);

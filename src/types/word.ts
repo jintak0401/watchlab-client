de0e204c5403eb5086734bdcb12480492530410e
jintak0401 <jintak0401@naver.com>
2023-03-30 01:58:28 +0900

    feat: Word 객체인지 확인하는 instanceOfWord 함수 구현 (#2)

--- src/types/word.ts
+++ src/types/word.ts
@@ -3,5 +3,9 @@ export interface Word {
   description: string;
 }
 
+export const instanceOfWord = (obj: any): obj is Word => {
+  return 'word' in obj && 'description' in obj;
+};
+
 export const glossaryChars = `@ABCDEFGHIJKLMNOPQRSTUVWXYZ#`.split('');
 export type GlossaryChar = (typeof glossaryChars)[number];

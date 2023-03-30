dbbe453bc42ff2606b45b7a725d9943f3ebd2535
jintak0401 <jintak0401@naver.com>
2023-03-30 13:15:57 +0900

    chore: localePath 값 추가

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     locales: ['en', 'ko', 'jp', 'zh'],
     defaultLocale: 'en',
     localeDetection: false,
+    localePath: path.resolve('./public/locales'),
   },
 };

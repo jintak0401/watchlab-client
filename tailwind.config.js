33f68e16edde6408158a8d9242efbefa04863e3c
jintak0401 <jintak0401@naver.com>
2023-04-08 12:49:43 +0900

    feat: 폰트 추가 (#23)

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,17 @@ module.exports = {
         electric: '#db00ff',
         ribbon: '#0047ff',
         'glossary-table': '#e2d1d1',
+        'study-cell': '#cdb5b6',
       },
       fontFamily: {
-        cormor: ['var(--font-cormorant-garamond)', ...fontFamily.sans],
+        cormor: ['var(--font-cormorant-garamond)', ...fontFamily.serif],
+        crimson: ['var(--font-crimson-text)', ...fontFamily.serif],
+        'crimson-pro': ['var(--font-crimson-pro)', ...fontFamily.serif],
+        inknut: ['var(--font-inknut-antiqua)', ...fontFamily.serif],
       },
       backgroundImage: {
         glossary: "url('/static/images/glossary/background.webp')",
+        study: "url('/static/images/study/background.webp')",
       },
     },
   },

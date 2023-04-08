15191090827e708e2bd057cade8acc933620c8bb
jintak0401 <jintak0401@naver.com>
2023-04-08 12:53:29 +0900

    feat: components/common 의 컴포넌트들을 모아서 export (#23)

---
+++ src/components/common/index.ts
@@ -0,0 +1,5 @@
+export { default as Description } from './Description';
+export { default as Grid } from './Grid';
+export { default as Subtitle } from './Subtitle';
+export { default as ThemeSwitch } from './ThemeSwitch';
+export { default as Title } from './Title';

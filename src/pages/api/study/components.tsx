010c2c5df3d31af9427275e80bb11154abca57e0
jintak0401 <jintak0401@naver.com>
2023-04-08 12:52:14 +0900

    feat: study 하위 페이지 추가 (#23)

---
+++ src/pages/api/study/components.tsx
@@ -0,0 +1,9 @@
+const ComponentsPage = () => {
+  return (
+    <div>
+      <h1>Components</h1>
+    </div>
+  );
+};
+
+export default ComponentsPage;
---
+++ src/pages/api/study/taxonomy.tsx
@@ -0,0 +1,9 @@
+const TaxonomyPage = () => {
+  return (
+    <div>
+      <h1>Taxonomy</h1>
+    </div>
+  );
+};
+
+export default TaxonomyPage;

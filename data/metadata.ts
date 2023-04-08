ba322275795514755ab69abf69644c72fca4ce31
jintak0401 <jintak0401@naver.com>
2023-04-08 12:51:44 +0900

    feat: study 페이지의 링크 리스트 metadata에 추가 (#23)

--- data/metadata.ts
+++ data/metadata.ts
@@ -1,8 +1,20 @@
 const metadata = {
   images: {
+    studyBg: '/static/images/study/background.webp',
     glossaryBg: '/static/images/glossary/background.webp',
     ghostFace: '/static/images/glossary/ghost-face.webp',
     loupes: '/static/images/glossary/loupes.webp',
+  },
+
+  study: {
+    links: [
+      'components',
+      'taxonomy',
+      'performance',
+      'functions',
+      'history',
+      'art',
+    ],
   },
   theme: 'light',
 };

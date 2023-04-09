4a701825da88ce2cc5095607603b59994b0dee23
jintak0401 <jintak0401@naver.com>
2023-04-09 11:49:37 +0900

    feat: Description 컴포넌트에 whitespace-pre-line 추가 (#24)

--- src/components/common/Description.tsx
+++ src/components/common/Description.tsx
@@ -1,11 +1,15 @@
 import styled from '@emotion/styled';
-import tw, { css } from 'twin.macro';
+import { PropsWithTwChildren } from 'react';
+import tw from 'twin.macro';
 
-const Description = styled.div(() => [
-  tw`text-center font-normal text-white text-3xl`,
-  css`
-    width: 1110px;
-  `,
+type Props = PropsWithTwChildren;
+
+const Description = ({ children, className }: Props) => {
+  return <DIV className={className}>{children}</DIV>;
+};
+
+const DIV = styled.div(() => [
+  tw`whitespace-pre-line text-center font-normal text-white text-3xl`,
 ]);
 
 export default Description;

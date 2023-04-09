bbfb33de5d97ce0805fa72c5ed33960ecd222353
jintak0401 <jintak0401@naver.com>
2023-04-09 09:03:22 +0900

    feat: Title 컴포넌트 폰트사이즈 변경 (#23)

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -1,11 +1,9 @@
 import styled from '@emotion/styled';
-import tw, { css } from 'twin.macro';
+import tw from 'twin.macro';
 
 const Title = styled.h1(() => [
-  tw`font-crimson font-bold text-white leading-none`,
-  css`
-    font-size: 100px;
-  `,
+  tw`font-crimson font-bold text-white text-8xl`,
+  tw`leading-none`,
 ]);
 
 export default Title;

c358336ce088db50495737564230c5b6cab169ff
jintak0401 <jintak0401@naver.com>
2023-04-09 10:26:51 +0900

    feat: manual 페이지 구현 (#24)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,6 +27,12 @@ const App = () => {
         >
           Go To Study
         </Link>
+        <Link
+          href="/manual"
+          tw="rounded-md bg-gray-200 px-4 py-2 text-center font-cormor font-bold text-2xl text-gray-900 hover:bg-gray-300"
+        >
+          Go To Manuals
+        </Link>
       </div>
     </div>
   );

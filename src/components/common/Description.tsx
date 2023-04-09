3e1f925dd90d26a581bd03c794a97cf33cc6b756
jintak0401 <jintak0401@naver.com>
2023-04-09 09:04:22 +0900

    feat: Description 컴포넌트 폰트사이즈 변경 (#23)

--- src/components/common/Description.tsx
+++ src/components/common/Description.tsx
@@ -2,10 +2,9 @@ import styled from '@emotion/styled';
 import tw, { css } from 'twin.macro';
 
 const Description = styled.div(() => [
-  tw`text-center font-normal text-white leading-10`,
+  tw`text-center font-normal text-white text-3xl`,
   css`
     width: 1110px;
-    font-size: 30px;
   `,
 ]);
 

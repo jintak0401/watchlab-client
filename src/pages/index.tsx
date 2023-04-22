2a786849a26b720a7ac5da854bc3ca8fb0af6823
jintak0401 <jintak0401@naver.com>
2023-04-22 18:34:16 +0900

    feat: 메인 페이지 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,51 @@
-import Link from 'next/link';
-import tw, { css } from 'twin.macro';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+import { GetServerSidePropsContext } from 'next';
+import { useRouter } from 'next/router';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import tw from 'twin.macro';
+import 'twin.macro';
 
-import ThemeSwitch from '@/components/common/ThemeSwitch';
+import Level1Layout from '@/components/layouts/Level1Layout';
 
-const App = () => {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const queryClient = new QueryClient();
+  const { locale = 'en' } = context;
+
+  /*
+  await queryClient.prefetchQuery(getGlossaryKey(locale), () =>
+    getGlossary(locale)
+  );
+
+  const [translations] = await Promise.all([
+    serverSideTranslations(locale, ['journal']),
+    queryClient.prefetchQuery(getGlossaryKey(locale), () =>
+      getGlossary(locale)
+    ),
+  ]);
+*/
+
+  const translations = await serverSideTranslations(locale, ['journal']);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+      ...translations,
+    },
+  };
+};
+
+const Index = () => {
+  const { locale = 'en' } = useRouter();
+  const { t } = useTranslation('journal');
+
   return (
-    <div
-      css={[
-        tw`flex h-screen w-screen items-center justify-center overflow-hidden bg-white dark:bg-gray-900`,
-        css`
-          transition: background-color 0.5s ease-in-out;
-        `,
-      ]}
-    >
-      <ThemeSwitch tw="fixed top-10 right-10" />
-      <div tw="flex flex-col gap-y-3">
-        <Link
-          href="/glossary"
-          tw="rounded-md bg-gray-200 px-4 py-2 text-center font-cormor font-bold text-2xl text-gray-900 hover:bg-gray-300"
-        >
-          Go To Glossary
-        </Link>
-        <Link
-          href="/study"
-          tw="rounded-md bg-gray-200 px-4 py-2 text-center font-cormor font-bold text-2xl text-gray-900 hover:bg-gray-300"
-        >
-          Go To Study
-        </Link>
-        <Link
-          href="/manual"
-          tw="rounded-md bg-gray-200 px-4 py-2 text-center font-cormor font-bold text-2xl text-gray-900 hover:bg-gray-300"
-        >
-          Go To Manuals
-        </Link>
-      </div>
-    </div>
+    <Level1Layout title={t('title')} subtitle={t('subtitle')} tw="bg-glossary">
+      <div css={[tw`bg-red-300`, { height: 3000 }]} />
+    </Level1Layout>
   );
 };
-export default App;
+
+export default Index;

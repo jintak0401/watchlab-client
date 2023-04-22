6e8098c6d1ed5ca9d069077a1683753a22b82751
jintak0401 <jintak0401@naver.com>
2023-04-22 18:42:58 +0900

    feat: study, manuals 이미지 추가

--- data/metadata.ts
+++ data/metadata.ts
@@ -7,6 +7,20 @@ const metadata = {
     logo: '/static/images/common/logo.webp',
     light: '/static/images/common/light.webp',
     dark: '/static/images/common/dark.webp',
+    study: [
+      '/static/images/study/10-1.png',
+      '/static/images/study/10-2.png',
+      '/static/images/study/10-3.png',
+      '/static/images/study/10-4.png',
+      '/static/images/study/10-5.png',
+      '/static/images/study/10-6.png',
+    ],
+    manuals: [
+      '/static/images/manual/11-1.png',
+      '/static/images/manual/11-2.png',
+      '/static/images/manual/11-3.png',
+      '/static/images/manual/11-4.png',
+    ],
   },
   study: {
     links: [

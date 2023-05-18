39e6a8e7af1d233caf2ca132be46cb87fb0229e9
jintak0401 <jintak0401@naver.com>
2023-05-18 18:15:41 +0900

    feat: 시계 구현 (#33)

--- src/components/common/Header/Clock.tsx
+++ src/components/common/Header/Clock.tsx
@@ -1,14 +1,79 @@
-import tw from 'twin.macro';
-import 'twin.macro';
+import styled from '@emotion/styled';
+import metadata from 'data/metadata';
+import NextImage from 'next/image';
+import tw, { css } from 'twin.macro';
 
-interface Props {
-  onClick: () => void;
-}
+import useClock from '@/hooks/useClock';
 
-const Clock = ({ onClick }: Props) => {
-  return <Button onClick={onClick}>clock</Button>;
+import { CLOCK_SIZE } from '@/utils/constants';
+import { calcClockRotate } from '@/utils/time';
+
+const Clock = () => {
+  const { plate, hourHand, minuteHand, secondHand } = metadata.images.clock;
+  const { hour, minute, second } = useClock();
+  const [hourRotate, minuteRotate, secondRotate] = calcClockRotate(
+    hour,
+    minute,
+    second
+  );
+
+  return (
+    <Container>
+      <NextImage
+        css={[
+          tw`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2`,
+          css`
+            width: ${CLOCK_SIZE}px;
+            height: ${CLOCK_SIZE}px;
+          `,
+        ]}
+        src={plate}
+        alt={'plate'}
+        width={1000}
+        height={1000}
+      />
+      <NextImage
+        css={getClockHandStyle(hourRotate, (CLOCK_SIZE * 2) / 3)}
+        src={hourHand}
+        alt={'hour'}
+        width={1000}
+        height={1000}
+      />
+      <NextImage
+        css={getClockHandStyle(minuteRotate)}
+        src={minuteHand}
+        alt={'minute'}
+        width={1000}
+        height={1000}
+      />
+      <NextImage
+        css={getClockHandStyle(secondRotate)}
+        src={secondHand}
+        alt={'second'}
+        width={1000}
+        height={1000}
+      />
+    </Container>
+  );
 };
 
-const Button = tw.button`flex h-20 w-20 items-start justify-center bg-red-200`;
+const getClockHandStyle = (rotate: number, handSize = CLOCK_SIZE - 30) => [
+  tw`absolute object-contain`,
+  css`
+    height: ${handSize}px;
+    top: 50%;
+    // 중앙을 중심으로 회전
+    transform-origin: 50% 0;
+    transform: rotate(${rotate}deg) translateY(-50%);
+  `,
+];
+
+const Container = styled.div(() => [
+  tw`relative aspect-square h-full`,
+  css`
+    width: ${CLOCK_SIZE}px;
+    height: ${CLOCK_SIZE}px;
+  `,
+]);
 
 export default Clock;

e5f7dac0283b91b2bf1a2e21cf45f42f671d5632
jintak0401 <jintak0401@naver.com>
2023-05-21 11:28:02 +0900

    feat: HeaderMain에서 timeout 제거 (#33)

--- src/components/common/Header/HeaderMain.tsx
+++ src/components/common/Header/HeaderMain.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import metadata from 'data/metadata';
 import NextImage from 'next/image';
 import Link from 'next/link';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import tw, { css } from 'twin.macro';
 
 import ButtonList from '@/components/common/Header/ButtonList';
@@ -13,39 +13,11 @@ import { CLOCK_SIZE } from '@/utils/constants';
 
 const HeaderMain = () => {
   const [openClockCollapse, setOpenClockCollapse] = useState(false);
-  const [showClock, setShowClock] = useState(true);
-  const timeout = useRef<NodeJS.Timeout>();
-
-  useEffect(() => {
-    const clockTimeout = setTimeout(() => {
-      setOpenClockCollapse((opened) => {
-        if (!opened) setShowClock(false);
-        return opened;
-      });
-    }, 5000);
-    return () => {
-      timeout.current && clearTimeout(timeout.current);
-      clearTimeout(clockTimeout);
-    };
-  }, []);
-
-  const collapseHandler = (isOpen: boolean) => {
-    setOpenClockCollapse(isOpen);
-    timeout.current && clearTimeout(timeout.current);
-    if (isOpen) {
-      setShowClock(isOpen);
-      return;
-    } else {
-      timeout.current = setTimeout(() => {
-        setShowClock(false);
-      }, 500);
-    }
-  };
 
   return (
     <>
       <Upside>
-        <ClockButton onClick={() => collapseHandler(!openClockCollapse)} />
+        <ClockButton onClick={() => setOpenClockCollapse((prev) => !prev)} />
         <Link href={'/'}>
           <NextImage
             src={metadata.images.logo}
@@ -59,7 +31,7 @@ const HeaderMain = () => {
       </Upside>
       <Hr />
       <ClockCollapse open={openClockCollapse}>
-        {showClock && <Clock />}
+        <Clock />
       </ClockCollapse>
     </>
   );

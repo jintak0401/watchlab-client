300e5d68a704e80d683fa3f72d40dd982e68c5c5
jintak0401 <jintak0401@naver.com>
2023-05-21 11:32:26 +0900

    feat: 400ms 이후에 date 업데이트 (#33)

--- src/components/common/Header/Clock.tsx
+++ src/components/common/Header/Clock.tsx
@@ -17,7 +17,7 @@ const ANIMATION: { [key in THandType]: string } = {
 
 const Clock = () => {
   const { plate, hourHand, minuteHand, secondHand } = metadata.images.clock;
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date(0));
   const [hourAngle, minuteAngle, secondAngle] = calcClockRotate(
     date.getHours(),
     date.getMinutes(),
@@ -28,7 +28,7 @@ const Clock = () => {
     const timeout = setTimeout(() => {
       const newDate = new Date();
       setDate(newDate);
-    }, 200);
+    }, 400);
     return () => {
       clearTimeout(timeout);
     };

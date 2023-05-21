5b65acbb9133438bedae792fe57737b61c991fb6
jintak0401 <jintak0401@naver.com>
2023-05-21 09:42:05 +0900

    feat: keyframe을 이용해 시계 애니메이션 구현 (#33)

--- src/components/common/Header/Clock.tsx
+++ src/components/common/Header/Clock.tsx
@@ -1,57 +1,28 @@
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 import metadata from 'data/metadata';
 import NextImage from 'next/image';
-import { useEffect, useRef } from 'react';
 import tw, { css } from 'twin.macro';
-
-import useClock from '@/hooks/useClock';
 
 import { CLOCK_SIZE } from '@/utils/constants';
 import { calcClockRotate } from '@/utils/time';
 
+type THandType = 'h' | 'm' | 's';
+
+const ANIMATION: { [key in THandType]: string } = {
+  h: '43200s infinite linear',
+  m: '3600s infinite linear',
+  s: '60s infinite linear',
+};
+
 const Clock = () => {
   const { plate, hourHand, minuteHand, secondHand } = metadata.images.clock;
-  const { hour, minute, second } = useClock();
-  const [hourRotate, minuteRotate, secondRotate] = calcClockRotate(
-    hour,
-    minute,
-    second
+  const date = new Date();
+  const [hourAngle, minuteAngle, secondAngle] = calcClockRotate(
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds()
   );
-  const handsRef = useRef<(HTMLDivElement | null)[]>([]);
-  const angleRef = useRef<[number, number, number]>([0, 0, 0]);
-
-  useEffect(() => {
-    const date = new Date();
-    const [hour, minute, second] = [
-      date.getHours(),
-      date.getMinutes(),
-      date.getSeconds(),
-    ];
-    const [hourAngle, minuteAngle, secondAngle] = calcClockRotate(
-      hour,
-      minute,
-      second
-    );
-    angleRef.current[0] = hourAngle;
-    angleRef.current[1] = minuteAngle;
-    angleRef.current[2] = secondAngle;
-    handsRef.current[0]!.style.transform = `rotate(${angleRef.current[0]}deg) translateY(-50%)`;
-    handsRef.current[1]!.style.transform = `rotate(${angleRef.current[1]}deg) translateY(-50%)`;
-    handsRef.current[2]!.style.transform = `rotate(${angleRef.current[2]}deg) translateY(-50%)`;
-
-    const interval = setInterval(() => {
-      angleRef.current[0] += 1 / 120;
-      angleRef.current[1] += 0.1;
-      angleRef.current[2] += 6;
-      handsRef.current[0]!.style.transform = `rotate(${angleRef.current[0]}deg) translateY(-50%)`;
-      handsRef.current[1]!.style.transform = `rotate(${angleRef.current[1]}deg) translateY(-50%)`;
-      handsRef.current[2]!.style.transform = `rotate(${angleRef.current[2]}deg) translateY(-50%)`;
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
 
   return (
     <Container>
@@ -69,24 +40,21 @@ const Clock = () => {
         height={1000}
       />
       <NextImage
-        ref={(ref) => (handsRef.current[0] = ref)}
-        css={getClockHandStyle(hourRotate, (CLOCK_SIZE * 2) / 3)}
+        css={getClockHandStyle('h', hourAngle, (CLOCK_SIZE * 2) / 3)}
         src={hourHand}
         alt={'hour'}
         width={1000}
         height={1000}
       />
       <NextImage
-        ref={(ref) => (handsRef.current[1] = ref)}
-        css={getClockHandStyle(minuteRotate)}
+        css={getClockHandStyle('m', minuteAngle)}
         src={minuteHand}
         alt={'minute'}
         width={1000}
         height={1000}
       />
       <NextImage
-        ref={(ref) => (handsRef.current[2] = ref)}
-        css={getClockHandStyle(secondRotate)}
+        css={getClockHandStyle('s', secondAngle)}
         src={secondHand}
         alt={'second'}
         width={1000}
@@ -96,14 +64,27 @@ const Clock = () => {
   );
 };
 
-const getClockHandStyle = (rotate: number, handSize = CLOCK_SIZE - 30) => [
+const rotate = (startAngle: number) => keyframes`
+  0% {
+    transform: rotateZ(${startAngle}deg) translateY(-50%);
+  }
+  100% {
+    transform: rotateZ(${startAngle + 360}deg) translateY(-50%);
+  }
+`;
+
+const getClockHandStyle = (
+  type: THandType,
+  angle: number,
+  handSize = CLOCK_SIZE - 30
+) => [
   tw`absolute object-contain`,
   css`
     height: ${handSize}px;
     top: 50%;
     transition: transform 0.5s;
     transform-origin: 50% 0;
-    transform: rotate(${rotate}deg) translateY(-50%);
+    animation: ${rotate(angle)} ${ANIMATION[type]};
   `,
 ];
 

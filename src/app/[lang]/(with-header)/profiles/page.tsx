1a0d8be9f0e6a1f9f91652b6cf1762c1c0005994
jintak0401 <jintak0401@naver.com>
2023-06-04 19:36:48 +0900

    feat: layout에서 page로 이동 (#41)

--- src/app/[lang]/(with-header)/profiles/page.tsx
+++ src/app/[lang]/(with-header)/profiles/page.tsx
@@ -1,10 +1,12 @@
 import 'twin.macro';
 
 import Hydration from '@/components/common/hydration';
+import WithThumbnailLayout from '@/components/layouts/with-thumbnail-layout';
 import ProfileChain from '@/components/profile/profile-chain';
 import ProfileList from '@/components/profile/profile-list';
 import ProfileSortButtons from '@/components/profile/profile-sort-buttons';
 
+import { useTranslation } from '@/i18n';
 import { getProfile } from '@/request/profile';
 import { PROFILE_KEY } from '@/utils/constants';
 
@@ -14,19 +16,24 @@ interface Props {
   params: { lang: Locale };
 }
 
-const ProfilesPage = ({ params: { lang } }: Props) => {
+const ProfilesPage = async ({ params: { lang } }: Props) => {
+  const { t } = await useTranslation(lang, 'profile');
   const queryKey = [PROFILE_KEY, lang];
   const queryFn = () => getProfile(lang);
 
   return (
-    <>
+    <WithThumbnailLayout
+      title={t('title')}
+      subtitle={t('subtitle')}
+      thumbnail={'profiles'}
+    >
       {/* @ts-expect-error Server Component */}
       <Hydration queryKey={queryKey} queryFn={queryFn}>
         <ProfileChain />
         <ProfileSortButtons />
         <ProfileList />
       </Hydration>
-    </>
+    </WithThumbnailLayout>
   );
 };
 

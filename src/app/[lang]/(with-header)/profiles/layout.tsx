edf0d3bc22d8b954a4ea743e7f1306d3f3cf94d6
jintak0401 <jintak0401@naver.com>
2023-06-04 19:33:33 +0900

    feat: /profiles 에 layout 추가 (#41)

--- src/app/[lang]/(with-header)/profiles/layout.tsx
+++ src/app/[lang]/(with-header)/profiles/layout.tsx
@@ -1,13 +1,18 @@
+import WithThumbnailLayout from '@/components/layouts/with-thumbnail-layout';
+
 import { RootProps } from '@/app/[lang]/layout';
 import { useTranslation } from '@/i18n';
 
 const ProfilesLayout = async ({ params: { lang }, children }: RootProps) => {
   const { t } = await useTranslation(lang, 'profile');
   return (
-    <div>
-      <span>{t('title')}</span>
+    <WithThumbnailLayout
+      title={t('title')}
+      subtitle={t('subtitle')}
+      thumbnail={'profiles'}
+    >
       {children}
-    </div>
+    </WithThumbnailLayout>
   );
 };
 

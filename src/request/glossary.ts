c711f56fceb4f21ecb69db1303366748785edcde
jintak0401 <jintak0401@naver.com>
2023-06-04 22:30:56 +0900

    feat: axios -> fetch 방식으로 변경 (#41)

--- src/request/glossary.ts
+++ src/request/glossary.ts
@@ -1,12 +1,19 @@
-import _axios from '@/request/instance';
+import siteMetadata from 'data/site-metadata';
+import process from 'process';
+
 import { Word } from '@/types';
 
 const glossaryReqUrl = (locale: string) => {
-  return `/${locale}/dictionary`;
+  return `${process.env.NEXT_PUBLIC_SERVER_URL}/${locale}/dictionary`;
 };
 
-export const getGlossary = async (locale: string) => {
+export const getGlossary = async (locale: string): Promise<Word> => {
   const url = glossaryReqUrl(locale);
-  const res = await _axios.get<(Word & { id: number })[]>(url);
-  return res?.data;
+  const res = await fetch(url, {
+    method: 'GET',
+    next: {
+      revalidate: siteMetadata.revalidate,
+    },
+  });
+  return res.json();
 };

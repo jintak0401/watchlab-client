66a3a053faee006ea341ba8d0d82e22728237d74
jintak0401 <jintak0401@naver.com>
2023-06-25 18:55:01 +0900

    feat: 헤더 하단의 중앙 부분, 디지털 시계 구현 (#47)

---
+++ src/components/common/header/date-display/digital-clock.tsx
@@ -0,0 +1,25 @@
+import tw from 'twin.macro';
+
+import useClock from '@/hooks/use-clock';
+
+import { padZero } from '@/utils/str-utils';
+
+const DigitalClock = () => {
+  const { hour, minute, second } = useClock();
+  const time = [hour, minute, second].map((num) => padZero(num)).join(':');
+  return (
+    <Time suppressHydrationWarning={true} dateTime={time}>
+      {time.split('').map((char, idx) => (
+        <TimeText key={idx} suppressHydrationWarning={true}>
+          {char}
+        </TimeText>
+      ))}
+    </Time>
+  );
+};
+
+const Time = tw.time`my-auto flex`;
+
+const TimeText = tw.div`inline-block flex-1 text-center font-franklin text-8xl`;
+
+export default DigitalClock;

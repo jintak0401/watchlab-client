0bb82c9e7b5e87958fbef4db799c034c08279e6e
jintak0401 <jintak0401@naver.com>
2023-06-25 18:42:08 +0900

    feat: 요일을 보여주는 컴포넌트 구현 (#47)

---
+++ src/components/common/header/date-display/calendar/day-of-week.tsx
@@ -0,0 +1,34 @@
+import { PropsWithTw, useReducer } from 'react';
+import tw from 'twin.macro';
+
+import useEveryMidnight from '@/hooks/use-every-midnight';
+
+import { DAY_OF_WEEK } from '@/utils/constants';
+
+const DayOfWeek = ({ className }: PropsWithTw) => {
+  const [, forceRerender] = useReducer((x) => x + 1, 0);
+  const dayOfWeek = new Date().getDay();
+  useEveryMidnight(forceRerender);
+  return (
+    <Container className={className}>
+      <div tw={'font-franklin text-4xl'}>{DAY_OF_WEEK[dayOfWeek]}</div>
+      <div tw={'flex w-full items-center text-4xl'}>
+        {DAY_OF_WEEK.map((day, idx) => (
+          <span
+            css={[
+              tw`inline-block grow text-center font-franklin`,
+              idx === dayOfWeek && tw`underline`,
+            ]}
+            key={day}
+          >
+            {day[0]}
+          </span>
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+const Container = tw.div`flex h-full flex-col items-center justify-center`;
+
+export default DayOfWeek;

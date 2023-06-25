663861e6f672218d93cea9cb6c04bdccf128220b
jintak0401 <jintak0401@naver.com>
2023-06-25 18:42:20 +0900

    feat: 월과 일을 보여주는 컴포넌트 구현 (#47)

---
+++ src/components/common/header/date-display/calendar/month-and-day.tsx
@@ -0,0 +1,23 @@
+import { PropsWithTw, useReducer } from 'react';
+import tw from 'twin.macro';
+
+import useEveryMidnight from '@/hooks/use-every-midnight';
+
+const MonthAndDay = ({ className }: PropsWithTw) => {
+  const [, forceRerender] = useReducer((x) => x + 1, 0);
+  const date = new Date();
+  const month = Intl.DateTimeFormat('en-US', { month: 'short' }).format(date);
+  const day = date.getDate();
+  useEveryMidnight(forceRerender);
+
+  return (
+    <Container className={className}>
+      <div tw={'font-franklin text-4xl'}>{day}</div>
+      <div tw={'font-franklin text-4xl'}>{month}</div>
+    </Container>
+  );
+};
+
+const Container = tw.div`flex h-full flex-col items-center justify-center`;
+
+export default MonthAndDay;

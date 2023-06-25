a4514f1f126f7ad615c49e8b98149049e7a2585d
jintak0401 <jintak0401@naver.com>
2023-06-25 22:44:54 +0900

    feat: 헤더 하단부 너비 및 간격 조정 (#47)

--- src/components/common/header/date-display/index.tsx
+++ src/components/common/header/date-display/index.tsx
@@ -1,15 +1,17 @@
 import { memo } from 'react';
-import tw from 'twin.macro';
+import tw, { styled } from 'twin.macro';
 
 import Calendar from '@/components/common/header/date-display/calendar';
 import CenturyAndLunar from '@/components/common/header/date-display/century-and-lunar';
 import DigitalClock from '@/components/common/header/date-display/digital-clock';
 import Timezone from '@/components/common/header/date-display/timezone';
 
+import { HEADER_BODY_WIDTH } from '@/styles/header';
+
 const DateDisplay = () => {
   return (
     <Container>
-      <div tw={'flex gap-4 pt-7 [&>*]:flex-1'}>
+      <div tw={'flex items-center gap-4 pt-7 [&>*]:flex-1'}>
         <Calendar />
         <DigitalClock />
         <CenturyAndLunar />
@@ -20,8 +22,10 @@ const DateDisplay = () => {
   );
 };
 
-const Container = tw.div`w-[2000px]`;
+const Container = styled.div`
+  width: ${HEADER_BODY_WIDTH}px;
+`;
 
-const Hr = tw.hr`w-full border-black border-t-2`;
+const Hr = tw.hr`my-1 w-full border-black border-t-2`;
 
 export default memo(DateDisplay);

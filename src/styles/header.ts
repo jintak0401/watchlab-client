f45c65add20b48a16fddebba642190e52a4317f2
jintak0401 <jintak0401@naver.com>
2023-06-25 22:42:21 +0900

    feat: 헤더 스타일 상수 수정 (#47)

--- src/styles/header.ts
+++ src/styles/header.ts
@@ -1,3 +1,4 @@
-export const MOON_SIZE = 90;
+export const MOON_SIZE = 65;
 export const CLOCK_SIZE = 300;
-export const CLOCK_COLLAPSE_HEIGHT = CLOCK_SIZE + 170;
+export const HEADER_BODY_WIDTH = 960;
+export const CLOCK_COLLAPSE_HEIGHT = CLOCK_SIZE + 200;

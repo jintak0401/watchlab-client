50bd01bedef0ea7568b34f9bc1efa4820bf61bee
jintak0401 <jintak0401@naver.com>
2023-06-25 21:35:49 +0900

    feat: 세계 시각 컴포넌트 적용 (#47)

--- src/components/common/header/clock-list/index.tsx
+++ src/components/common/header/clock-list/index.tsx
@@ -1,17 +1,26 @@
-import tw from 'twin.macro';
+import tw, { css, styled } from 'twin.macro';
+
+import WorldClock from '@/components/common/header/clock-list/world-clock';
+
+import { CLOCK_SIZE } from '@/styles/header';
 
 import Clock from './clock';
 
 const ClockList = () => {
   return (
     <Container>
-      <div>1</div>
+      <WorldClock />
       <Clock />
       <div>3</div>
     </Container>
   );
 };
 
-const Container = tw.div`flex items-center justify-center`;
+const Container = styled.div(() => [
+  tw`flex items-center justify-center`,
+  css`
+    column-gap: ${CLOCK_SIZE / 10}px;
+  `,
+]);
 
 export default ClockList;

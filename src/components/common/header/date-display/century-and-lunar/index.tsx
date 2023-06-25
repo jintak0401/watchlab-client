9ce062cc6a9f74dcfd718f4521ad4638e0cbe2f8
jintak0401 <jintak0401@naver.com>
2023-06-25 18:57:05 +0900

    feat: 헤더 하단의 우측 부분 구현 (#47)

---
+++ src/components/common/header/date-display/century-and-lunar/index.tsx
@@ -0,0 +1,21 @@
+import tw from 'twin.macro';
+
+import CenturyDisplay from '@/components/common/header/date-display/century-and-lunar/century-display';
+import LeapYearDisplay from '@/components/common/header/date-display/century-and-lunar/leap-year-display';
+import LunarDisplay from '@/components/common/header/date-display/century-and-lunar/lunar-display';
+import YearDisplay from '@/components/common/header/date-display/century-and-lunar/year-display';
+
+const CenturyAndLunar = () => {
+  return (
+    <Container>
+      <YearDisplay />
+      <CenturyDisplay />
+      <LeapYearDisplay />
+      <LunarDisplay />
+    </Container>
+  );
+};
+
+const Container = tw.div`flex h-full items-center justify-center [&>*]:flex-1`;
+
+export default CenturyAndLunar;
